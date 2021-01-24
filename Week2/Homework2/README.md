ddc2a4aacb2e333d5b49a46cb72ad20d25e47105
Kartik Aiyer <kartik.aiyer@keeptruckin.com>
2021-01-23 20:29:40 -0800

    Week2 Homework2

---
+++ Week2/Homework2/README.md
@@ -0,0 +1,22 @@
+# Build Instructions
+
+Uses the same style CMakeLists.txt file that is used by the course. Point to local openCV installation using `OpenCV_DIR`.
+
+Standard CMake build
+```bash
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. 
+make
+```
+The binary `mouse_annotation` will be created in the build directory.
+
+# Executable Provided
+
+A prebuilt binary for Ubuntu 20.04 on an amd64 system is provided in `cmake-build-debug/mouse_annotation`. 
+
+# Resulting Cropped Image
+
+The resulting cropped image will appear in `Data/cropped_sample.jpg`. The input image is hardcoded to `Data/sample.jpg`
+
+In addition to cropped image file, a cropped version will also be displayed.
---
+++ Week2/Homework2/submission.cpp
@@ -0,0 +1,51 @@
+//
+// Created by kartik on 1/23/21.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+static cv::Point2d s_point1, s_point2;
+static cv::Mat s_img;
+void drawBoundingBox(int action, int x, int y, int flags, void* userdata)
+{
+  if(action == cv::EVENT_LBUTTONDOWN)
+  {
+    s_point1.x = x;
+    s_point1.y = y;
+  } else if(action == cv::EVENT_LBUTTONUP)
+  {
+    s_point2.x = x;
+    s_point2.y = y;
+    cv::rectangle(s_img, s_point1, s_point2, cv::Scalar{0,255,0}, 2, cv::LINE_AA);
+    cv::imshow("Window", s_img);
+    cv::Mat crop = s_img(cv::Range(s_point1.y, s_point2.y), cv::Range(s_point1.x, s_point2.x));
+    cv::imwrite(DATA_PATH "/cropped_sample.jpg", crop);
+    cv::imshow("Cropped", crop);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  s_img = cv::imread(DATA_PATH "/sample.jpg");
+  if(s_img.data)
+  {
+    cv::namedWindow("Window");
+    cv::setMouseCallback("Window", drawBoundingBox);
+    int k = 0;
+    while(k != 27) {
+      cv::putText(s_img,
+                  "Click and Drag a Bounding box around the face, press Esc to exit",
+                  cv::Point(10, s_img.rows - 20),
+                  cv::FONT_HERSHEY_SIMPLEX,
+                  0.45,
+                  cv::Scalar(255,255,255),
+                  1, cv::LINE_AA);
+      cv::imshow("Window", s_img);
+      k = cv::waitKey() & 0xFF;
+    }
+  } else {
+    std::cout << "Couldn't open sample.jpg" << std::endl;
+  }
+  return 0;
+}
