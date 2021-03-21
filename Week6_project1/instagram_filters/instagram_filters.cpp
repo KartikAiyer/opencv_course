1a862349f47bec039a2a5eb67936982bae512235
Kartik <kartik.aiyer@gmail.com>
2021-03-21 14:32:56 -0700

    Formatted code

--- Week6_project1/instagram_filters/instagram_filters.cpp
+++ Week6_project1/instagram_filters/instagram_filters.cpp
@@ -5,8 +5,15 @@
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <exception>
 
-int main(int argc, void** argv)
+int main(int argc, char **argv)
 {
-    
+  cv::Mat image;
+  std::string filename{DATA_PATH "/trump.jpg"};
+  std::cout << filename << std::endl;
+  image = cv::imread(filename);
+
+  cv::imshow("Image", image);
+  cv::waitKey();
 }
